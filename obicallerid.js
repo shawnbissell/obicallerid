6e937d58cd8cb13c8a2b66dabb656e496ea54f4e
Shawn <shawn.bissell@gmail.com>
2012-08-26 15:26:47 -0700

    incrementing version to 0.1.1 adding regex to parse caller id infor from "[SLIC] CID to deliver" message as well as Sip From header better error handling cleaner logs

--- obicallerid.js
+++ obicallerid.js
@@ -29,7 +29,8 @@ var growl = require('growl');
 var opencnam = require('opencnam');
 var dgram = require("dgram");
 var applescript = require("applescript");
-var fromRegex = /^INVITE[\S\s]*From:\s*"?([\w\s\.\+\?\$]*?)"?<sip:.*?(\d*)@?[\.\d]*:\d*>;.*/;
+var fromRegex = /^INVITE[\S\s]*From:\s*"?([\w\s\.\+\?\$]*?)"?<sip:((.*)@)?(.*)>;.*/;
+var cidRegex = /<7> \[SLIC\] CID to deliver: '([\w\s\.\+\?\$]*?)' (\d*).*/;
 
 function cleanNumber(num)
 {
@@ -103,56 +104,111 @@ function lookupCnam(number)
     }
 }
 
-var server = dgram.createSocket("udp4");
+var lastSentTime = new Date();
 
-server.on("message", function (msg, rinfo) {
-    logger.verbose("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
-    if(msg) {
-        var s = new String(msg);
-        var matches = fromRegex.exec(s);
-        if (matches) {
-            var number = matches[2];
-            var fromName  = matches[1];
-            logger.info("Caller ID Number found! " + number );
-            if (fromName != "") {
-                logger.info("Caller ID Name found! " + fromName );
-                growl('Incoming call from \n' + fromName + ' ' + number, { title: 'Obi Caller ID'} );
-            } else {
-                lookupCnam(number)
+function sendCallerIDInfo(name, number, ipAddress)
+{
+    logger.info("Caller ID Number found! " + number );
+    var currentTime = new Date();
+    if(currentTime.getTime() - lastSentTime.getTime() < 1000){
+        logger.info("Preventing duplicate messages for " + number );
+        return;
+    }
+    lastSentTime = currentTime;
+
+
+    if (name != "") {
+        logger.info("Caller ID Name found! " + name );
+        if(number != undefined){
+            logger.info("Sending name and number  " + name + " " + number);
+            growl('Incoming call from \n' + name + ' ' + number,  { title: 'Obi Caller ID'} );
+        } else {
+            logger.info("Sending name and ipAddress  " + name + " " + ipAddress);
+            growl('Incoming call from \n' + name + ' <' + ipAddress + ">", { title: 'Obi Caller ID'} );
+        }
+    } else {
+        lookupCnam(number)
+    }
+}
+
+
+try {
+
+
+    var server = dgram.createSocket("udp4");
+
+    server.on("message", function (msg) {
+        try {
+            if(msg) {
+                var logMessage = new String(msg);
+                if(logMessage.indexOf("<7> [SLIC] Command:") == -1 && logMessage.indexOf("<7> XMPP:") == -1 ) {
+                    logger.verbose(logMessage);
+                } else {
+                    return;
+                }
+
+                var cidMatches = cidRegex.exec(logMessage);
+                if (cidMatches) {
+                    sendCallerIDInfo(cidMatches[1], cidMatches[2]);
+                }
+                var fromMatches = fromRegex.exec(logMessage);
+                if (fromMatches) {
+                    sendCallerIDInfo(fromMatches[1], fromMatches[3], fromMatches[4]);
+                }
             }
         }
+        catch(ex){
+            logger.error("Error OnMessage! " + ex);
+        }
+
+    });
+
+    server.on("listening", function () {
+        var address = server.address();
+        logger.info("server listening " + address.address + ":" + address.port);
+    });
+
+    server.bind(7000);
+    growl('Obi Caller Id Started!\nListening on Port 7000', { title: 'Obi Caller ID'} );
+
+
+    /* unit tests */
+    if(command == "test"){
+        var cidTest1 = "<7> [SLIC] CID to deliver: '' 5556011212";
+        var cidMatch1 = cidRegex.exec(cidTest1);
+        logger.info("cidMatch1 " + cidMatch1[1] + " " + cidMatch1[2]);
+        var cidTest2 = "<7> [SLIC] CID to deliver: 'some name' 1645845823";
+        var cidMatch2 = cidRegex.exec(cidTest2);
+        logger.info("cidMatch2 " + cidMatch2[1] + " " + cidMatch2[2]);
+
+        var test = "INVITE sip:16045551111@192.168.1.130:5063 SIP/2.0 \nFrom: \"test.?+$ name_1\"<sip:6045551212@208.65.240.165:5060>;tag=ecaasd4kr5vomv.o Call-ID: D547EAC6@28.73.12.66~o";
+        var test2 = "INVITE sip:16045551111@192.168.1.130:5063 SIP/2.0 \nFrom: <sip:6045551212@208.65.240.165>;tag=ecaasd4kr5vomv.o Call-ID: D547EAC6@28.73.12.66~o";
+        //var test3 = "INVITE asdasd \nFrom: \"Anonymous\"<sip:208.65.240.165:5060>;tag=gkvhwppdzniwszml.o";
+        var test3 = "INVITE To: <sip:16045551212@208.65.240.165>\n                From: \"Anonymous\"<sip:208.65.240.165>;tag=2pqeabu2uswl2q32.o";
+
+        var testmatches = fromRegex.exec(test);
+        logger.info("test regex 1 " + testmatches[1] + " " + testmatches[3]);
+        var testmatches2 = fromRegex.exec(test2);
+        logger.info("test regex 2 " + testmatches2[1] + " " + testmatches2[3]);
+        var testmatches3 = fromRegex.exec(test3);
+        logger.info("test regex 3 " + testmatches3[1] + " " + testmatches3[3]);
+        var testnum = cleanNumber("0111(345) 898 9888 x123");
+        logger.info("test cleanNumber " + testnum);
+
+        lookupOpenCnam("6045551234abc");
+        setTimeout(function() {
+            lookupCnam("604 555 1234");
+            lookupCnam("604 555 4321");
+            lookupCnam("604 555 3214");
+            lookupCnam("604 555 2341");
+            lookupCnam("604(555)x1111x")
+
+        }, 30000);
+
     }
-});
-
-server.on("listening", function () {
-    var address = server.address();
-    logger.info("server listening " + address.address + ":" + address.port);
-});
-
-server.bind(7000);
-growl('Obi Caller Id Started!\nListening on Port 7000', { title: 'Obi Caller ID'} );
-
-
-/* unit tests */
-if(command == "test"){
-    var test = "INVITE sip:16045551111@192.168.1.130:5063 SIP/2.0 \nFrom: \"test.?+$ name_1\"<sip:6045551212@208.65.240.165:5060>;tag=ecaasd4kr5vomv.o Call-ID: D547EAC6@28.73.12.66~o";
-    var test2 = "INVITE sip:16045551111@192.168.1.130:5063 SIP/2.0 \nFrom: <sip:6045551212@208.65.240.165:5060>;tag=ecaasd4kr5vomv.o Call-ID: D547EAC6@28.73.12.66~o";
-    var test3 = "INVITE asdasd \nFrom: \"Anonymous\"<sip:208.65.240.165:5060>;tag=gkvhwppdzniwszml.o";
-    var testmatches = fromRegex.exec(test);
-    logger.info("test regex 1 " + testmatches[1] + " " + testmatches[2]);
-    var testmatches2 = fromRegex.exec(test2);
-    logger.info("test regex 2 " + testmatches2[1] + " " + testmatches2[2]);
-    var testmatches3 = fromRegex.exec(test3);
-    logger.info("test regex 3 " + testmatches3[1] + " " + testmatches3[2]);
-    var testnum = cleanNumber("0111(345) 898 9888 x123");
-    logger.info("test cleanNumber " + testnum);
-    lookupOpenCnam("6045551234abc");
-    setTimeout(function() {
-        lookupCnam("604 555 1234");
-        lookupCnam("604 555 4321");
-        lookupCnam("604 555 3214");
-        lookupCnam("604 555 2341");
-        lookupCnam("604(555)x1111x")
-
-    }, 30000);
 }
+catch(ex)
+{
+    logger.error("Fatal Error occurred! " + ex + "\n" + ex);
+}
+
